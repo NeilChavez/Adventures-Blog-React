99fa621e67fc872c81d7ce067d72b1b1885f0478
NeilChavez <lolochavez22@icloud.com>
2022-10-19 22:57:49 +0200

    Add styles to PickedArticle Component

--- src/components/PickedArticle.js
+++ src/components/PickedArticle.js
@@ -1,18 +1,26 @@
+import "./PickedArticle.css";
+
 export default function PickedArticle() {
   return (
-    <section>
+    <section className="section-picked-article">
       <div className="container">
         <h3>Titolo</h3>
         <article className="picked-article">
-          <h4 className="picked-article-title">subtitle</h4>
-          <p className="picked-aritcle-paragraph">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia,
-            mollitia reprehenderit perspiciatis possimus ipsa fuga nihil
-            corporis aspernatur atque, nostrum, expedita consequuntur ad
-            obcaecati aut odio harum! Iste, at sed?
-          </p>
+          <div className="picked-article-wrapper">
+            <h4 className="picked-article-title">subtitle</h4>
+            <p className="picked-aritcle-paragraph">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia,
+              mollitia reprehenderit perspiciatis possimus ipsa fuga nihil
+              corporis aspernatur atque, nostrum, expedita consequuntur ad
+              obcaecati aut odio harum! Iste, at sed?
+            </p>
+          </div>
           <figure className="picked-aritcle-figure">
-            <img  className="icked-aritcle-image" src="./assets/citiesImages/barcelona01.jpeg" alt="titolo" />
+            <img
+              className="icked-aritcle-image"
+              src="./assets/citiesImages/barcelona01.jpeg"
+              alt="titolo"
+            />
           </figure>
         </article>
       </div>
