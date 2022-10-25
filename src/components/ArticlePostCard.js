52cbde09a1cd3f917ecf15a6817170e48d71f75a
NeilChavez <lolochavez22@icloud.com>
2022-10-25 23:52:48 +0200

    Change HeroSection Component

--- src/components/ArticlePostCard.js
+++ src/components/ArticlePostCard.js
@@ -1,10 +1,11 @@
 import "./ArticlePostCard.css";
 
-export default function ArticlePostCard({ textArticle }) {
+export default function ArticlePostCard({ textArticle, img }) {
+  let ulrImage = img || "https://placeimg.com/300/200/any";
   return (
     <article className="article-post">
       <figure className="article-figure">
-        <img src="https://placeimg.com/300/200/any" className="article-image"alt="dummy img" />
+        <img src={ulrImage} className="article-image" alt="dummy img" />
       </figure>
       <figcaption className="article-figcaption">
         <div className="article-date">22 August</div>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "../App.css";
-import HeroImage from "./HeroImage";
 import "./HeroSection.css";
+import ArticlePostCard from "./ArticlePostCard";
 
-export default function HeroSection({ urlImageHero }) {
-
+export default function HeroSection() {
   return (
     <section className="hero-section">
-       <HeroImage urlImageHero={urlImageHero}/>
+      <article className="hero-grid">
+        <ArticlePostCard
+          textArticle="Titulo en el hero"
+          img={"./assets/citiesImages/venezia02.jpeg"}
+        />
+        <div>
+          <ArticlePostCard
+            textArticle="Titulo en el hero"
+            img={"./assets/citiesImages/venezia02.jpeg"}
+          />
+          <ArticlePostCard
+            textArticle="Titulo en el hero"
+            img={"./assets/citiesImages/venezia02.jpeg"}
+          />
+        </div>
+      </article>
     </section>
-  )
+  );
 }
