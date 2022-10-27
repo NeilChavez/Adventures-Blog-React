3e1e8148d78529d332f767761d6a0869414a0837
NeilChavez <lolochavez22@icloud.com>
2022-10-27 23:44:38 +0200

    Add Recent Posts to HeroSection Component

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,14 @@ import React from "react";
 import "../App.css";
 import "./HeroSection.css";
 import ArticlePostCard from "./ArticlePostCard";
+import RecentPosts from "./RecentPosts";
+import SmallCard from "./SmallCard";
+
+let articles = [
+  "Back Home After Trip with Friends",
+  "First Real Attempt at Ballet Dance",
+  "Winning Makes me More Humble",
+];
 
 export default function HeroSection() {
   return (
@@ -12,15 +20,20 @@ export default function HeroSection() {
           img={"./assets/citiesImages/venezia02.jpeg"}
         />
 
-          <ArticlePostCard
-            textArticle="Titulo en el hero"
-            img={"./assets/citiesImages/venezia02.jpeg"}
-          />
-          <ArticlePostCard
-            textArticle="Titulo en el hero"
-            img={"./assets/citiesImages/venezia02.jpeg"}
-          />
+        <ArticlePostCard
+          textArticle="Titulo en el hero"
+          img={"./assets/citiesImages/venezia02.jpeg"}
+        />
+        <ArticlePostCard
+          textArticle="Titulo en el hero"
+          img={"./assets/citiesImages/venezia02.jpeg"}
+        />
       </article>
+      <RecentPosts>
+        {articles.map((article) => {
+          return <SmallCard key={article}></SmallCard>;
+        })}
+      </RecentPosts>
     </section>
   );
 }
