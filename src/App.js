35cad57d48631a6f65876443698828e776cd518c
NeilChavez <lolochavez22@icloud.com>
2022-10-30 21:20:13 +0100

    Add styles variables to AppComponent

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-
         <Route path="/products" element={<h1>Products page</h1>} />
         <Route path="/sign-up" element={<h1>Sing-up Page</h1>} />
       </Routes>
